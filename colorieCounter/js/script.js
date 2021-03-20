30d118db7af866b5aad46194a1d29ea078e55da2
SashaBianco <belov_aleks1995@mail.ru>
2021-03-20 19:06:47 +0300

    1.0.1
    improved code readability.
    variable names changed.
    changes were made to the structure of the document.

--- colorieCounter/js/script.js
+++ colorieCounter/js/script.js
@@ -1,92 +1,49 @@
-var ageElement      =  document.getElementById("age");
-var heightElement   =  document.getElementById("height");
-var weightElement   =  document.getElementById("weight");
-var submitButton    =  document.querySelector(".form__submit-button");
-var resetButton     =  document.querySelector(".form__reset-button");
-var activity        =  document.getElementsByName('activity');
-var gender          =  document.getElementsByName('gender');
-var sectionElement  =  document.querySelector('section');
-var inputElement    =  document.getElementsByClassName('input__wrapper');
+var ageInput                =  document.getElementById('age');
+var heightInput             =  document.getElementById('height');
+var weightInput             =  document.getElementById('weight');
+var submitButton            =  document.querySelector('.form__submit-button');
+var resetButton             =  document.querySelector('.form__reset-button');
+var activityRadioButtons    =  document.getElementsByName('activity');
+var genderRadioButtons      =  document.getElementsByName('gender');
+var summaryBlock           =  document.querySelector('section');
+var inputElement            =  document.getElementsByClassName('input__wrapper');
 
-var calorieСounter = function () {
-    onlyNumbers();
-    if (ageElement.value == "" && heightElement.value == "" && weightElement.value == "") {
-        resetButton.setAttribute("disabled", "disabled");
-    }   else {
-        resetButton.removeAttribute('disabled');
-        if (ageElement.value !== "" && heightElement.value !== "" && weightElement.value !== "") {
-            submitButton.removeAttribute("disabled");
-        }   else {
-            submitButton.setAttribute("disabled", "disabled");
-        };
-    };
+var checkingEmptyFields =  function (elementsValue) {
+    for (var i=1; i<elementsValue.length; i++) {
+        if (elementsValue[i].checked) {
+            elementsValue[i].checked = 'false';
+        }  
+    }
+    elementsValue[0].checked = 'true';   
 };
 
 var resetValue = function () {
-    ageElement.value = "";
-    heightElement.value = "";
-    weightElement.value = "";
-    checkingEmptyFields(gender);
-    checkingEmptyFields(activity); 
-    sectionElement.classList.add('counter__result--hidden');
-    resetButton.setAttribute("disabled", "disabled");
-    submitButton.setAttribute("disabled", "disabled");
-}
-
-var checkingEmptyFields =  function (elementsValue) {
-    for (var i=0; i<elementsValue.length; i++) {
-        if (elementsValue[i].checked = 'true') {
-            elementsValue[i].checked = 'false';
-        };   
-    };
-    elementsValue[0].checked = 'true';   
-} 
-
-
-var showResult = function () {
-    sectionElement.classList.remove('counter__result--hidden');
-    calculationResults();
-}
+    ageInput.value = "";
+    heightInput.value = "";
+    weightInput.value = "";
+    checkingEmptyFields(genderRadioButtons);
+    checkingEmptyFields(activityRadioButtons); 
+    summaryBlock.classList.add('counter__result--hidden');
+    resetButton.setAttribute('disabled', 'disabled');
+    submitButton.setAttribute('disabled', 'disabled');
+};
 
 var onlyNumbers = function () {
     for (var i=0; i<inputElement.length; i++) {
         var inputElementValue = inputElement[i].children[0].value;
         inputElementValue = inputElementValue.replace (/\D/g, '');
-    };
-} 
-
-var calculationResults = function() {
-    var male = document.getElementById('gender-male');
-    var N;
-    var WeightSupport;
-    var WeightGain;
-    var WeightLoss;
-
-    if  (male.checked) {
-            N = (10 * weightElement.value)+ (6.25 * heightElement.value) - (5 * ageElement.value) + 5;
-    }   else {
-            N = (10 * weightElement.value) + (6.25 * heightElement.value) - (5 * ageElement.value) - 161;
-    };
-
-    getCoefficient();
-
-    WeightSupport = Math.round(N * coefficient);
-    WeightGain = Math.round((WeightSupport + (WeightSupport * 0.15)));
-    WeightLoss = Math.round((WeightSupport - (WeightSupport * 0.15)));
-
-    document.getElementById('calories-norm').innerHTML = WeightSupport;
-    document.getElementById('calories-minimal').innerHTML = WeightGain;
-    document.getElementById('calories-maximal').innerHTML = WeightLoss;
-}    
+    }
+};
 
 var getCoefficient = function() {
-    for (var i = 0; i < activity.length; i++) {
-        var x;
-        if (activity[i].checked) {
+    var x;
+    var coefficient;
+    for (var i = 0; i < activityRadioButtons.length; i++) {
+        if (activityRadioButtons[i].checked) {
             x = i;
-        };
-    };
-
+            break;
+        }
+    }
     switch (x) {
         case 0: 
             coefficient = 1.2;
@@ -103,6 +60,57 @@ var getCoefficient = function() {
         case 4: 
             coefficient = 1.9;
             break;
+        default:
+            coefficient = 0;
+            break;
     };
     return coefficient;
-}
+};
+
+var calculationResults = function() {
+    var maleRadioButton = document.getElementById('gender-male');
+    var caloriesForWeightMaintain = (10 * weightInput.value)+ (6.25 * heightInput.value) - (5 * ageInput.value);
+    var WeightSupport;
+
+    if  (maleRadioButton.checked) {
+        caloriesForWeightMaintain += 5;
+    }   else {
+        caloriesForWeightMaintain -= 161;
+    }
+
+    getCoefficient();
+    WeightSupport = caloriesForWeightMaintain * getCoefficient();
+
+    document.getElementById('calories-norm').innerHTML = Math.round(WeightSupport);
+    document.getElementById('calories-minimal').innerHTML = Math.round(1.15 * WeightSupport);
+    document.getElementById('calories-maximal').innerHTML = Math.round(0.85 * WeightSupport);
+};  
+
+var showResult = function () {
+    calculationResults();
+    summaryBlock.classList.remove('counter__result--hidden');
+};
+
+var calorieСounter = function () {
+    onlyNumbers();
+    if (ageInput.value == "" && heightInput.value == "" && weightInput.value == "") {
+        resetButton.setAttribute('disabled', 'disabled');
+    }   else {
+        resetButton.removeAttribute('disabled');
+        if (ageInput.value !== "" && heightInput.value !== "" && weightInput.value !== "") {
+            submitButton.removeAttribute('disabled');
+        }   else {
+            submitButton.setAttribute('disabled', 'disabled');
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
