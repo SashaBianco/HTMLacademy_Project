e5419ac433cdd82689c26f5404d285d816e9433e
SashaBianco <belov_aleks1995@mail.ru>
2021-03-30 13:54:10 +0300

    1.0.2
    Add functions for endless scrolling of cards (by 5 card)

--- infinityCarousel/js/script.js
+++ infinityCarousel/js/script.js
@@ -1,11 +1,12 @@
 let parent = document.querySelector(".slider__list");
 let children = parent.children;
-let index = 0;
 
 let next = function () {
-    parent.append(children[index]);
+    let i = 0;
+    parent.append(children[i],children[i+1],children[i+2],children[i+3],children[i+4]);
 }
 
 let prev = function () {
-    parent.prepend(children[children.length - 1]);
+    let length = children.length;
+    parent.prepend(children[length - 5],children[length - 4],children[length - 3],children[length - 2],children[length - 1]);
 }
